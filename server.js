03b6682bbd53d4b8e8ee49023ce1bad0a32fcdf7
Nicholas Sladden <nickoliasxii@gmail.com>
2018-02-01 18:06:07 -0600

    just styling

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const app = express();
 const exphbs = require('express-handlebars');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3006;
 
 app.use(express.static("public"));
 
